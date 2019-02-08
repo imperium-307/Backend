f26f1dab097e566efe7c1353f2f84cd0a2fe55f5
Peter Reckamp <preckam@purdue.edu>
2019-02-07 20:08:32 -0500

    Configured Firebase

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const dotenv = require('dotenv');
+const firebase = require("firebase");
 
 const app = express()
 
@@ -8,6 +9,13 @@ dotenv.load({ path: '.env' });
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }));
+var config = {
+	apiKey: process.env.FIREBASE_KEY,
+	authDomain: "imperium-ab01e.firebaseapp.com",
+	databaseURL: "https://imperium-ab01e.firebaseio.com",
+	storageBucket: "imperium-ab01e.appspot.com",
+};
+firebase.initializeApp(config);
 
 const rootRouter = require('./routers/index');
 
