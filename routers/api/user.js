178f850130f86c50c086c2a6a6ef7e55ffc52e0c
CJ Enright <cenrigh@purdue.edu>
2019-02-10 17:28:57 -0500

    Adds user authentication through JWTs

--- routers/api/user.js
+++ routers/api/user.js
@@ -1,61 +1,87 @@
 const router = require('express').Router()
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 router.post('/', (req, res, next) => {
 	// TODO firebase interactions for getting the current user based on JWT
-	return res.status(200)
+	return res.status(200).json(req.token)
 })
 
 router.post('/signup', (req, res, next) => {
-	var email = req.body.email
-	var password = req.body.password
-	var passwordConfirm = req.body.passwordConfirm
+	// Make sure there's not already a valid JWT
+	if (req.token == null) {
+		var email = req.body.email
+		var password = req.body.password
+		var passwordConfirm = req.body.passwordConfirm
 
-	// Make sure email is at least x@x.x
-	if (email.length < 5) {
-		// TODO we can throw some errors in here with .json({err: "thing"})
-		return res.status(422)
-	}
-
-	if (password != passwordConfirm) {
-		// TODO we can throw some errors in here with .json({err: "thing"})
-		return res.status(422)
-	}
-
-	bcrypt.hash(password, 7, (err, hash) => {
-		if (err) {
+		// Make sure email is at least x@x.x
+		if (email.length < 5) {
 			// TODO we can throw some errors in here with .json({err: "thing"})
-			return res.status(500)
+			return res.status(422)
 		}
 
-		// TODO firebase interactions for creating user
-		// That will also involve making sure emails are unique
-		// ^ should be an attribute you can set for the emails column
-		// TODO respond with JWT
+		if (password != passwordConfirm) {
+			// TODO we can throw some errors in here with .json({err: "thing"})
+			return res.status(422)
+		}
 
-		// TODO attach JWT and other state stuff here with .json({})
-		return res.status(200)
-	})
+		bcrypt.hash(password, 7, (err, hash) => {
+			if (err) {
+				// TODO we can throw some errors in here with .json({err: "thing"})
+				return res.status(500)
+			}
 
+			// TODO firebase interactions for creating user
+			// That will also involve making sure emails are unique
+			// ^ should be an attribute you can set for the emails column
+
+			return res.status(200).json({
+				token: makeJWT(email)
+			})
+		})
+	} else {
+		// We'll just refresh the token for now...
+		// Depending on the app structure we might want to redirect
+		return res.status(200).json({
+			token: makeJWT(email)
+		})
+	}
 })
 
 router.post('/login', (req, res, next) => {
-	var email = req.body.email
-	var password = req.body.password
+	// Make sure there's not already a valid JWT
+	if (req.token == null) {
+		var email = req.body.email
+		var password = req.body.password
 
-	// TODO get password hash from firebase
-	// var hash = firebase.getUser()
+		// TODO get password hash from firebase
+		// var hash = firebase.getUser() <- for example
 
-	bcrypt.compare(password, hash, function(err, res) {
-		if(res) {
-			// TODO attach JWT and other state stuff here with .json({})
-			return res.status(200)
-		} else {
-			// TODO we can throw some errors in here with .json({err: "thing"})
-			return res.status(403)
-		} 
-	});
-
+		bcrypt.compare(password, hash, function(err, res) {
+			if(res) {
+				return res.status(200).json({
+					token: makeJWT(email)
+				})
+			} else {
+				// TODO we can throw some errors in here with .json({err: "thing"})
+				return res.status(403)
+			} 
+		});
+	} else {
+		// We'll just refresh the token for now...
+		// Depending on the app structure we might want to redirect
+		return res.status(200).json({
+			token: makeJWT(email)
+		})
+	}
 })
+
+function makeJWT(email) {
+	return jwt.sign({
+		// TODO once firebase interactions are figured out
+		//id: id,
+		email: email
+	}, process.env.JWT_SECRET)
+}
 
 module.exports = router
--- routers/index.js
+++ routers/index.js
@@ -1,6 +1,20 @@
 const router = require('express').Router()
+const jwt = require('jsonwebtoken');
 const apiRouter = require('./api/index')
 
+router.use(setJWT)
 router.use('/api', apiRouter)
+
+function setJWT(req, res, next) {
+	jwt.verify(req.body.token, process.env.JWT_SECRET, function(err, dec) {
+		if (err) {
+			req.token = null
+		} else {
+			req.token = dec
+		}
+
+		next()
+	})
+}
 
 module.exports = router
