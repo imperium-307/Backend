5be9f7c5e0617d0b53f0db288bad6ac0337dfbb8
Jeremy-Potratz <jeremyottopotratz@gmail.com>
2019-02-10 18:58:32 -0500

    Dumby code for adding users

--- app.js
+++ app.js
@@ -24,3 +24,40 @@ app.use('/', rootRouter)
 app.listen(3000, () => {
   console.log('%s Express server listening on port %d', '✓', 3000);
 });
+
+//Get a database reference to our blog
+const db = firebase.database();
+const ref = db.ref("server/saving-data/fireblog");
+const usersRef = ref.child("users");
+usersRef.set({
+	user0: {
+		name: "Jon Bon Jovi",
+		dob: "March 2, 1962",
+		college: "Purdue University"
+	},
+	user1: {
+		name: "Don Quixote",
+		dob: "March 5, 1612",
+		college: "Indiana University"
+	}
+
+});
+const hopperRef = usersRef.child("user0");
+hopperRef.update({
+	"name": "Shawn White"
+});
+//Completion Callback example
+usersRef.set("I'm writing data", function(error) {
+  if (error) {
+    alert("Data could not be saved." + error);
+  } else {
+    alert("Data saved successfully.");
+  }
+});
+
+const name = ref.on("value", function(snapshot) {
+	console.log(snapshot.val());
+}, function (errorObject) {
+	//console.log("The read failed: " + errorObject.code);
+});
+//can also change "value" to be when a child_removed or child_changed
