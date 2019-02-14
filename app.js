33e2a32a5a3f04daaf78506eb6faa8fe7a853400
Jeremy-Potratz <jeremyottopotratz@gmail.com>
2019-02-13 22:33:02 -0500

    Added some errors and removed broken code in app.js

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const dotenv = require('dotenv');
-const firebase = require("firebase");
+const firebase = require('firebase');
+const admin = require('firebase-admin');
+const functions = require('firebase-functions');
 
 const app = express()
-
 dotenv.load({ path: '.env' });
 
 app.use(bodyParser.json())
@@ -24,40 +25,3 @@ app.use('/', rootRouter)
 app.listen(3000, () => {
   console.log('%s Express server listening on port %d', '✓', 3000);
 });
-
-//Get a database reference to our blog
-const db = firebase.database();
-const ref = db.ref("server/saving-data/fireblog");
-const usersRef = ref.child("users");
-usersRef.set({
-	user0: {
-		name: "Jon Bon Jovi",
-		dob: "March 2, 1962",
-		college: "Purdue University"
-	},
-	user1: {
-		name: "Don Quixote",
-		dob: "March 5, 1612",
-		college: "Indiana University"
-	}
-
-});
-const hopperRef = usersRef.child("user0");
-hopperRef.update({
-	"name": "Shawn White"
-});
-//Completion Callback example
-usersRef.set("I'm writing data", function(error) {
-  if (error) {
-    alert("Data could not be saved." + error);
-  } else {
-    alert("Data saved successfully.");
-  }
-});
-
-const name = ref.on("value", function(snapshot) {
-	console.log(snapshot.val());
-}, function (errorObject) {
-	//console.log("The read failed: " + errorObject.code);
-});
-//can also change "value" to be when a child_removed or child_changed
--- routers/api/user.js
+++ routers/api/user.js
@@ -1,14 +1,31 @@
 const router = require('express').Router()
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const firebase = require('firebase');
+const admin = require('firebase-admin');
+//const functions = require('firebase-functions');
+
+var db = firebase.database();
+var ref = db.ref();
+//var usersRef = ref.child('users');
+ref.child('users').push({
+	jeremyPotratz: {
+		college: 'Purdue University',
+		dob: 'Jan 4, 1999',
+		name: 'Jeremy Potratz'
+	}
+});
+
 
 router.post('/', (req, res, next) => {
 	// TODO firebase interactions for getting the current user based on JWT
+	//TODO with jacob on 2/14
 	return res.status(200).json(req.token)
 })
 
 router.post('/signup', (req, res, next) => {
 	// Make sure there's not already a valid JWT
+	//TODO with jacob on 2/14
 	if (req.token == null) {
 		var email = req.body.email
 		var password = req.body.password
@@ -17,22 +34,37 @@ router.post('/signup', (req, res, next) => {
 		// Make sure email is at least x@x.x
 		if (email.length < 5) {
 			// TODO we can throw some errors in here with .json({err: "thing"})
+			throw new Error('Invalid Email');
 			return res.status(422)
 		}
 
 		if (password != passwordConfirm) {
 			// TODO we can throw some errors in here with .json({err: "thing"})
+			throw new Error('Passwords do not match each other.');
 			return res.status(422)
 		}
 
 		bcrypt.hash(password, 7, (err, hash) => {
 			if (err) {
 				// TODO we can throw some errors in here with .json({err: "thing"})
-				return res.status(500)
-			}
+	 		}
+
+		firebase.auth().createUserWithEmailAndPassword('Jeremy', 'jp@x.com')
+    .catch(function(error) {
+  	// Handle Errors here.
+  	var errorCode = error.code;
+  	var errorMessage = error.message;
+  	if (errorCode == 'auth/weak-password') {
+    	alert('The password is too weak.');
+  	} else {
+    	alert(errorMessage);
+  	}
+  	console.log(error);
+		});
 
 			// TODO firebase interactions for creating user
 			// That will also involve making sure emails are unique
+			// check through emails? i feel like that is redundant though
 			// ^ should be an attribute you can set for the emails column
 
 			return res.status(200).json({
@@ -65,7 +97,7 @@ router.post('/login', (req, res, next) => {
 			} else {
 				// TODO we can throw some errors in here with .json({err: "thing"})
 				return res.status(403)
-			} 
+			}
 		});
 	} else {
 		// We'll just refresh the token for now...
